c46ebab1e1cc7ffe52508d0d965c9bdc41db4846
Mehrzad Sotoodeh <mehrzad@ethika.com>
2016-04-04 12:10:16 -0700

    added lineSmooth option for interpolation of threshold path

--- dist/chartist-plugin-threshold.js
+++ dist/chartist-plugin-threshold.js
@@ -31,14 +31,17 @@
       maskNames: {
         aboveThreshold: 'ct-threshold-mask-above',
         belowThreshold: 'ct-threshold-mask-below'
-      }
+      },
+      lineSmooth: Chartist.Interpolation.none({
+        fillHoles: true
+      })
     };
 
-    function createMasks(data, thresholdSeries, options) {
+    function createMasks(chart, thresholdSeries, options) {
       // Select the defs element within the chart or create a new one
-      var defs = data.svg.querySelector('defs') || data.svg.elem('defs');
-      var width = data.svg.width();
-      var height = data.svg.height();
+      var defs = chart.svg.querySelector('defs') || chart.svg.elem('defs');
+      var width = chart.svg.width();
+      var height = chart.svg.height();
 
       // Create mask for upper part above threshold
       var aboveMask = defs
@@ -61,15 +64,13 @@
         });
 
       if (thresholdSeries.data.length) {
-        // TODO add thresholdSmooth option to plugin and use here
-        var interpolator = Chartist.Interpolation.none({
-          fillHoles: true
-        });
-
-        var thresholdSvgPath = createThresholdSvgPath(data, thresholdSeries.data, interpolator);
+        // generate SVG path for the threshold series and append to each mask
+        var interpolator = options.lineSmooth;
+        var thresholdSvgPath = createThresholdSvgPath(chart, thresholdSeries.data, interpolator);
         var firstElement = thresholdSvgPath.pathElements[0];
         var lastElement = thresholdSvgPath.pathElements[thresholdSvgPath.pathElements.length - 1];
 
+        // first element is removed and replaced with a line from top-left of the chart to the first point
         var aboveMaskSvgPath = thresholdSvgPath.clone(true)
           .position(0)
           .remove(1)
@@ -78,6 +79,7 @@
           .position(thresholdSvgPath.pathElements.length + 1)
           .line(lastElement.x, 0);
 
+        // first element is removed and replaced with a line from bottom-left of the chart to the first point
         var belowMaskSvgPath = thresholdSvgPath
           .position(0)
           .remove(1)
@@ -100,15 +102,10 @@
       return defs;
     }
 
+    // generates an SVG Path for the given threshold data and returns an instance of Chartist.Svg.Path
     function createThresholdSvgPath(chart, thresholdData, interpolator) {
       var pathCoordinates = [];
       var pathData = [];
-
-      var normalizedData = Chartist.normalizeData({
-        series: [thresholdData]
-      });
-
-      thresholdData = Chartist.getDataArray(normalizedData, chart.options.reverseData, true).shift();
 
       thresholdData.forEach(function(value, valueIndex) {
         var p = {
@@ -127,13 +124,23 @@
       return interpolator(pathCoordinates, pathData);
     }
 
+    // converts threshold parameter to a data series, normalizes data, and returns threshold series
     function normalizeThresholdData(threshold, chart) {
       var thresholdSeries;
 
       if (Chartist.isNum(threshold)) {
-        var data = Chartist.times(chart.data.labels.length).map(function() {
-          return threshold;
-        });
+        // if threshold is a static number, generate a series of static data points
+        var data = [];
+
+        if (chart.data.series.length) {
+          var count = chart.data.series.reduce(function(prev, curr) {
+            return Math.max(prev, (curr.data || curr).length);
+          }, 0);
+
+          data = Chartist.times(count).map(function() {
+            return threshold;
+          });
+        }
 
         thresholdSeries = {
           data: data,
@@ -158,6 +165,11 @@
         }
       }
 
+      var normalizedData = Chartist.normalizeData({
+        series: [thresholdSeries]
+      });
+
+      thresholdSeries.data = Chartist.getDataArray(normalizedData, chart.options.reverseData, true).shift();
       return thresholdSeries;
     }
 
@@ -172,7 +184,7 @@
 
           chart.on('draw', function (data) {
             // if the element represents the control series, don't apply threshold classes or masks to it
-            if (data.series && data.series.name === thresholdSeries.name) {
+            if (data.series && data.series.name && data.series.name === thresholdSeries.name) {
               return;
             }
 
@@ -180,8 +192,9 @@
               // For points we can just use the data value and compare against the threshold in order to determine
               // the appropriate class
 
-              // use value of array at given index as the threshold for the current point
-              var thresholdValue = Number(thresholdSeries.data[data.index] || 0);
+              // use point at given index in the threshold data as the threshold for the current point
+              var thresholdPoint = thresholdSeries.data[data.index] || {y: 0};
+              var thresholdValue = +thresholdPoint.y || 0;
 
               data.element.addClass(
                 data.value.y >= thresholdValue ? options.classNames.aboveThreshold : options.classNames.belowThreshold
